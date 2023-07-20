8e9843854a45c3d834b309f05da1cb882e05ad7a
root <root@LAPTOP-CLJLRK5D>
2023-07-20 10:39:46 -0300

    added binary parser

--- src/parser.c
+++ src/parser.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <assert.h>
 #include <errno.h>
+#include <endian.h>
+#include <math.h>
 #include "parser.h"
 
 int text_parser(const char *buf, char *toks[TEXT_MAX_TOKS], int lens[TEXT_MAX_TOKS]) {
@@ -27,5 +29,66 @@ int text_parser(const char *buf, char *toks[TEXT_MAX_TOKS], int lens[TEXT_MAX_TO
 	}
 
 	log(3, "checking '%s', ntok = %i", toks[0], ntok);
+	return ntok;
+}
+
+unsigned char* get_arg(unsigned char* buf) {
+	int len;
+	memmove(&len, buf,4);
+	len = ntohl(len);
+	buf = buf + 4;
+
+	unsigned char* arg = malloc(len);
+	assert(arg);
+	arg = memmove(arg, buf, len);
+
+	return arg;
+}
+
+
+int bin_parser(unsigned char *buf, char* toks[TEXT_MAX_TOKS]) {
+	unsigned char* p = buf;
+	unsigned char* command = p;
+	p++;
+	int ntok = 0;
+	switch((int) *command) {
+		case 11: // PUT
+			toks[ntok++] = command;
+			unsigned char* arg1 = get_arg(p);
+			toks[ntok++] = arg1;
+
+			int len;
+			memmove(&len, p,4);
+			len = ntohl(len);
+			p = p + (4 + len);
+
+			unsigned char* arg2 = get_arg(p);
+			toks[ntok++] = arg2;
+			printf("PUT %s %s\n",arg1, arg2);
+			break;
+
+		case 12 : //DEL
+			toks[ntok++] = "DEL";
+			unsigned char* arg = get_arg(p);
+			toks[ntok++] = arg;
+			printf("DEL %s\n",arg);
+			break;
+
+		case 13: // GET
+			toks[ntok++] = "GET";
+			arg = get_arg(p);
+			toks[ntok++] = arg;
+			printf("GET %s\n",arg);
+			break;
+
+		case 21:
+			toks[ntok++] = "STATS";
+			printf("STATS \n");
+			break;
+			
+		default:
+			printf("Command not recognized\n");
+			return 1;
+	}
 	return ntok;
 }
--- src/parser.h
+++ src/parser.h
@@ -12,4 +12,22 @@
 //! @param[out] ntok - cantidad de tokens
 int text_parser(const char *buf, char *toks[3], int *lens );
 
+
+
+//! @brief Parser de binario.
+//! 
+//! @param[in] buf - unsigned char*. 
+//! @param[out] toks - char *: arreglo de tokens 
+//! @return ntok - cantidad de tokens
+int bin_parser(unsigned char *buf, char *toks[3]);
+
+
+//! @brief Función auxiliar para parser binario. Dado un array de bytes
+//! toma los primeros 4 bytes para conseguir la longitud del argumento a recuperar.
+//! Luego toma los siguientes n bytes, donde n es la cantidad de bytes que marca la longitud.  
+//!
+//! @param[in] buf - unsigned char*: array de bytes  
+//! @return arg - unsigned char*: argumento leido
+unsigned char* get_arg(unsigned char* buf);
+
 #endif
