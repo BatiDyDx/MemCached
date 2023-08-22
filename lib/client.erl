f1117c8cf70360ed698e7d9c57dc6234950c0078
JuanFigueredo-C <juan.b.figueredo01@gmail.com>
2023-08-21 21:00:49 -0300

    erlang client almost finished

--- lib/client.erl
+++ lib/client.erl
@@ -14,24 +14,55 @@ start(Hostname, IsPriv) ->
             Port = 8889
     end,
     spawn(fun() ->
-        {ok, Socket} = gen_tcp:connect(Hostname,Port,[{reuseaddr, true},{active,true}]),
+        %activo
+        %{ok, Socket} = gen_tcp:connect(Hostname,Port,[{reuseaddr, true},{active,true},binary]),
+        %pasivo
+        % TENEMOS QUE CAPTURAR ERROR DE CONEXION
+        {ok, Socket} = gen_tcp:connect(Hostname,Port,[{reuseaddr, true},{active,false},binary]),
         worker_thread(Socket)
     end).
 
 worker_thread(Socket) ->
+    % -------------------- PASIVO ---------------------
     receive
-        {tcp, Socket, Data} -> 
-            io:fwrite("> ~s~n",[Data]),
-            worker_thread(Socket);
         {send, Cmd} -> case gen_tcp:send(Socket, Cmd) of
-                            ok -> io:fwrite("Enviado!~n");
+                            ok -> get_ans(Socket, false);
                             {error, Reason} -> io:fwrite("Error: ~s~n", [Reason])
                         end,
                         worker_thread(Socket);
-        {tcp_closed, _} -> io:fwrite("Conexion terminada~n"),
-                           gen_tcp:close(Socket);
-        exit -> gen_tcp:close(Socket)
+        {send_stats, Cmd} -> case gen_tcp:send(Socket, Cmd) of
+                                ok -> get_ans(Socket, true);
+                                {error, Reason} -> io:fwrite("Error: ~s~n", [Reason])
+                            end;
+        {tcp_closed, _} -> close_conn(Socket);
+        exit -> close_conn(Socket)
     end.
+    % ------------------ ACTIVO -----------------
+    % receive
+    %     {tcp, Socket, Data} -> 
+    %         parse_data(Data, Socket);
+    %     {send, Cmd} -> case gen_tcp:send(Socket, Cmd) of
+    %                         ok -> io:fwrite("Enviado!~n");
+    %                         {error, Reason} -> io:fwrite("Error: ~s~n", [Reason])
+    %                     end,
+    %                     worker_thread(Socket);
+    %     {tcp_closed, _} -> io:fwrite("Conexion terminada~n"),
+    %                        gen_tcp:close(Socket);
+    %     exit -> gen_tcp:close(Socket)
+    % end.
+
+close_conn(Socket) ->
+    io:fwrite("Conexión terminada"),
+    gen_tcp:close(Socket).
+
+get_ans(Socket, Stats) ->
+    case gen_tcp:recv(Socket,0) of
+    {ok, Data} ->
+        parse_data(Data, Stats);
+    {error, closed} ->
+        close_conn(Socket)
+    end,
+    worker_thread(Socket).
 
 put(Id, Key, Value) ->
     Bkey = term_to_binary(Key),
@@ -55,8 +86,40 @@ del(Id, Key) ->
 
 stats(Id) ->
     Cmd = <<?STATS:8>>,
-    Id ! {send, Cmd}.
+    Id ! {send_stats, Cmd}.
 
 exit(Id) ->
     Id ! exit.
 
+parse_data(Data, Stats) ->
+    Nbyte = byte_size(Data),
+    io:fwrite("byte data ~p~n",[Nbyte]),
+    case Data of 
+        <<101,_:32,Rest/binary>> ->
+            case Rest of
+                <<>> -> io:fwrite("ok vacio~n");
+                _ -> if 
+                       Stats -> SVal = Rest;
+                       true -> SVal = binary_to_term(Rest)
+                     end,
+                     io:fwrite("OK ~p~n",[SVal])
+            end;
+        <<101,_:32,Stats>> ->
+            io:fwrite("OK ~s~n",[Stats]);
+        <<101>> ->
+            io:fwrite("OK~n");
+        <<111>> ->
+            io:fwrite("EINVALID~n");
+        <<112>> ->
+            io:fwrite("ENOTFOUND~n");
+        <<113>> ->
+            io:fwrite("EBINARY~n");
+        <<114>> ->
+            io:fwrite("EBIG~n");
+        <<115>> ->
+            io:fwrite("EUNK~n");
+        <<116>> ->
+            io:fwrite("EOOM~n");
+        _ ->
+            io:fwrite("MESSAGE ERROR~n")
+    end.
